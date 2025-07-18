84e97c7a329306a43ea10b192bbe43736291d069
senthil <apsenthilkumar2001@gmail.com>
2025-07-18 06:45:10 +0530

    496 solution

---
+++ 496-next-greater-element/Solution.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Build the map using Monotonic Stack
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Build the result
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
